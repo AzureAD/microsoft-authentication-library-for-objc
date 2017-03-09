73304b35b16269fdecb0ba0ae5de4f160b2615a5
Jason Kim <Jason@Jasons-MBP.home>
2017-03-09 14:57:41 -0800

    add initial target and settings

---
+++ MSAL/test/automation/ios/MSALAutoMainViewController.h
@@ -0,0 +1,13 @@
+//
+//  MSALAutoMainViewController.h
+//  MSAL
+//
+//  Created by Jason Kim on 3/9/17.
+//  Copyright © 2017 Microsoft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MSALAutoMainViewController : UIViewController
+
+@end
---
+++ MSAL/test/automation/ios/MSALAutoMainViewController.m
@@ -0,0 +1,37 @@
+//
+//  MSALAutoMainViewController.m
+//  MSAL
+//
+//  Created by Jason Kim on 3/9/17.
+//  Copyright © 2017 Microsoft. All rights reserved.
+//
+
+#import "MSALAutoMainViewController.h"
+
+@interface MSALAutoMainViewController ()
+
+@end
+
+@implementation MSALAutoMainViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
