e0d8b6ffae6fbbd34ac82f89eb28c8bccca5b2c1
Ryan Pangrle <rpangrle@microsoft.com>
2017-05-03 18:34:24 -0700

    Cause debugger to automatically break when UT network response not found

--- MSAL/test/unit/utils/MSALTestURLSession.m
+++ MSAL/test/unit/utils/MSALTestURLSession.m
@@ -38,6 +38,46 @@
 #import "NSString+MSALHelperMethods.h"
 #import "NSURL+MSALExtensions.h"
 
+#include <assert.h>
+#include <stdbool.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <sys/sysctl.h>
+
+// From https://developer.apple.com/library/content/qa/qa1361/_index.html
+static bool AmIBeingDebugged(void)
+// Returns true if the current process is being debugged (either
+// running under the debugger or has a debugger attached post facto).
+{
+    int                 junk;
+    int                 mib[4];
+    struct kinfo_proc   info;
+    size_t              size;
+    
+    // Initialize the flags so that, if sysctl fails for some bizarre
+    // reason, we get a predictable result.
+    
+    info.kp_proc.p_flag = 0;
+    
+    // Initialize mib, which tells sysctl the info we want, in this case
+    // we're looking for information about a specific process ID.
+    
+    mib[0] = CTL_KERN;
+    mib[1] = KERN_PROC;
+    mib[2] = KERN_PROC_PID;
+    mib[3] = getpid();
+    
+    // Call sysctl.
+    
+    size = sizeof(info);
+    junk = sysctl(mib, sizeof(mib) / sizeof(*mib), &info, &size, NULL, 0);
+    assert(junk == 0);
+    
+    // We're being debugged if the P_TRACED flag is set.
+    
+    return ( (info.kp_proc.p_flag & P_TRACED) != 0 );
+}
+
 @implementation MSALTestURLResponse
 
 + (MSALTestURLResponse *)requestURLString:(NSString *)requestUrlString
@@ -385,6 +425,13 @@ static NSMutableArray *s_responses = nil;
                 return response;
             }
         }
+    }
+    
+    if (AmIBeingDebugged())
+    {
+        // This will cause the tests to immediately stop execution right here if we're in the debugger,
+        // hopefully making it a little easier to see why a test is failing. :)
+        __builtin_trap();
     }
     
     // This class is used in the test target only. If you're seeing this outside the test target that means you linked in the file wrong
