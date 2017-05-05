9d6373a71b559ee1a52df35034266ffcdc1df8d0
Ryan Pangrle <rpangrle@microsoft.com>
2017-05-05 16:13:28 -0700

    Remove Static Lib target and fix xcconfigs
    - Remove quote marks around “@rpath” in DYLIB_INSTALL_NAME_BASE, this was causing issues when dropping a framework built from our the project into another project, including when using Carthage
    - Remove the static library build
    - Add a clean unit test host app

---
+++ MSAL/test/unit/ios/unit-test-host/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  unit-test-host
+//
+//  Created by Ryan Pangrle on 5/5/17.
+//  Copyright © 2017 Microsoft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
