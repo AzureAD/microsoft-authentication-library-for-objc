4807993df54aa6c4f79a61862b5666fccc134e4d
Jason Zeng <zeyong@microsoft.com>
2018-09-12 21:19:00 -0700

    Make destination configurable

--- ReleaseArchive.sh
+++ ReleaseArchive.sh
@@ -2,18 +2,19 @@
 
 if [ -z "$1" ]
   then
-    echo -e "\n\rUsage: ReleaseArchive [release tag] \n\rE.g. ReleaseArchive 0.1.6\n\r"
+    echo -e "\r\nUsage:\r\nReleaseArchive <release tag> [destination]\r\nE.g. ReleaseArchive 0.1.6\r\nor\r\nReleaseArchive 0.1.6 /Users/abc/Downloads\r\n(Zip and Tar file will be output to ~\Desktop if not specified.)\r\n"
     exit
 fi
 
 libraryName=microsoft-authentication-library-for-objc
 tag=$1
+if [ -z "$2" ]; then destination=~/Desktop; else destination=$2; fi
 releaseFolder=$libraryName-$tag
 
 # archive the main project without submodule and uncompress it
-git archive --prefix=$releaseFolder/ -o ~/Desktop/$mainProj.tar HEAD
-tar -xf ~/Desktop/$mainProj.tar -C ~/Desktop/
-rm ~/Desktop/$mainProj.tar
+git archive --prefix=$releaseFolder/ -o $destination/$mainProj.tar HEAD
+tar -xf $destination/$mainProj.tar -C $destination/
+rm $destination/$mainProj.tar
 
 repoPath=`pwd`
 
@@ -24,17 +25,17 @@ git submodule foreach | while read omittedChars submodulePath; do
         submodulePath=${submodulePath%\'}
         
         cd $submodulePath
-        git archive --prefix=$releaseFolder/$submodulePath/ HEAD > ~/Desktop/tmp.tar
+        git archive --prefix=$releaseFolder/$submodulePath/ HEAD > $destination/tmp.tar
         
         
-        tar -xf ~/Desktop/tmp.tar -C ~/Desktop/
-        rm ~/Desktop/tmp.tar
+        tar -xf $destination/tmp.tar -C $destination/
+        rm $destination/tmp.tar
 done
 
 # create the target tar/zip file
-cd ~/Desktop
+cd $destination
 tar -cf ./$releaseFolder.tar ./$releaseFolder
 zip -r -q -X ./$releaseFolder.zip ./$releaseFolder
 rm -R ./$releaseFolder
 
-echo "Release tar/zip have been created and put at ~/Desktop"
+echo -e "\r\nDone!\r\n\r\n$releaseFolder.tar\r\nand\r\n$releaseFolder.zip\r\nhave been created at $destination.\r\n"
