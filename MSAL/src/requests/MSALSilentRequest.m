5ffce23485dde766f24e6f174216f13c9623d788
Roman Khrestek <rokhrest@microsoft.com>
2018-11-19 19:16:06 -0800

    Comments addressed

--- MSAL/src/requests/MSALSilentRequest.m
+++ MSAL/src/requests/MSALSilentRequest.m
@@ -341,7 +341,10 @@
     MSIDRefreshToken *refreshToken = _familyRefreshToken ? _familyRefreshToken : _refreshToken;
     return [[MSIDAADRefreshTokenGrantRequest alloc] initWithEndpoint:[self tokenEndpoint]
                                                             clientId:_parameters.clientId
-                                                        enrollmentId:[_authority enrollmentIdForHomeAccountId:_parameters.account.homeAccountId.identifier legacyUserId:_parameters.account.username]
+                                                        enrollmentId:[_authority enrollmentIdForHomeAccountId:_parameters.account.homeAccountId.identifier
+                                                                                                 legacyUserId:_parameters.account.username
+                                                                                                      context:nil
+                                                                                                        error:nil]
                                                                scope:[[self requestScopes:nil] msidToString]
                                                         refreshToken:[refreshToken refreshToken]
                                                               claims:[self claims]
