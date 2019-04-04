ccd7e09449d26431c63b9ce940bbb69bdbac153c
Rohit Narula <ronaru@microsoft.com>
2019-04-03 23:50:40 -0700

    add acquireToken view

---
+++ MSAL/test/app/mac/AcquireTokenViewController.m
@@ -0,0 +1,22 @@
+//
+//  AcquireTokenViewController.m
+//  MSALMacTestApp
+//
+//  Created by Rohit Narula on 4/3/19.
+//  Copyright © 2019 Microsoft. All rights reserved.
+//
+
+#import "AcquireTokenViewController.h"
+
+@interface AcquireTokenViewController ()
+
+@end
+
+@implementation AcquireTokenViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do view setup here.
+}
+
+@end
---
+++ MSAL/test/app/mac/SettingsViewController.h
@@ -0,0 +1,17 @@
+//
+//  SettingsViewController.h
+//  MSALMacTestApp
+//
+//  Created by Rohit Narula on 4/3/19.
+//  Copyright © 2019 Microsoft. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface SettingsViewController : NSViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
