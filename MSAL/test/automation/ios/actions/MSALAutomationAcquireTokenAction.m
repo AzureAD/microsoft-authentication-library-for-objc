9bee9d2982a458daba19b4c5b977a83b94c4304b
Olga Dalton <oldalton@microsoft.com>
2019-08-22 11:10:01 -0700

    Fixed MSAL automation

--- MSAL/test/automation/ios/actions/MSALAutomationAcquireTokenAction.m
+++ MSAL/test/automation/ios/actions/MSALAutomationAcquireTokenAction.m
@@ -130,7 +130,14 @@
         acquireTokenAuthority = [MSALAuthority authorityWithURL:authorityUrl error:nil];
     }
     
-    MSALWebviewParameters *webviewParameters= [[MSALWebviewParameters alloc] initWithParentViewController:containerController];
+    UIViewController *parentController = containerController;
+    
+    if (containerController.presentedViewController)
+    {
+        parentController = containerController.presentedViewController;
+    }
+    
+    MSALWebviewParameters *webviewParameters= [[MSALWebviewParameters alloc] initWithParentViewController:parentController];
     
     MSIDWebviewType webviewSelection = testRequest.webViewType;
     
