0f49232b63eb128dd78fd9fa9bfce22cca73dba4
Rohit Narula <ronaru@microsoft.com>
2019-09-04 15:51:11 -0700

    Added cache viewer to MSAL MacOS test app

--- MSAL/test/app/mac/MSALCacheViewController.h
+++ MSAL/test/app/mac/MSALCacheViewController.h
@@ -26,13 +26,10 @@
 //------------------------------------------------------------------------------
 
 #import <Cocoa/Cocoa.h>
-#import "Credential.h"
 
 NS_ASSUME_NONNULL_BEGIN
 
 @interface MSALCacheViewController : NSViewController <NSOutlineViewDataSource, NSOutlineViewDelegate>
-
-@property NSMutableArray<Credential *> *credentials;
 
 @end
 
