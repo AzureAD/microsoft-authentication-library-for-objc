ec09401ca31941a5decfb10e66cdcc9ba7d1fbaa
Olga Dalton <oldalton@microsoft.com>
2019-09-05 22:20:59 -0700

    Updated script

--- build_docs.sh
+++ build_docs.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
-echo -e "Copying MSAL public files"
-mkdir docs.temp
-mkdir docs.temp/MSAL
-cp `find MSAL/src/public` docs.temp/MSAL
-cd docs.temp
-jazzy --objc --umbrella-header MSAL/MSAL.h --framework-root . --sdk iphonesimulator
+if [ "$TRAVIS_BRANCH" == "reference-docs" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ]
+then
+  gem install jazzy
+  echo -e "Copying MSAL public files"
+  mkdir docs.temp
+  mkdir docs.temp/MSAL
+  cp `find MSAL/src/public` docs.temp/MSAL
+  cd docs.temp
+  echo -e "Generating MSAL documentation"
+  jazzy --objc --umbrella-header MSAL/MSAL.h --framework-root . --sdk iphonesimulator
+fi
