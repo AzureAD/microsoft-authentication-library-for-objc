089875a4380adf54d4ceeda6263144280516af4a
Olga Dalton <oldalton@microsoft.com>
2019-09-06 18:40:47 -0700

    Updated docs generation script

--- build_docs.sh
+++ build_docs.sh
@@ -7,7 +7,8 @@ then
   mkdir docs.temp
   mkdir docs.temp/MSAL
   cp `find MSAL/src/public` docs.temp/MSAL
+  cp README.md docs.temp/
   cd docs.temp
   echo -e "Generating MSAL documentation"
-  jazzy --objc --umbrella-header MSAL/MSAL.h --framework-root . --sdk iphonesimulator
+  jazzy --objc --umbrella-header MSAL/MSAL.h --framework-root . --sdk iphonesimulator --author Microsoft\ Corporation --author_url https://aka.ms/azuread  --github_url https://github.com/AzureAD/microsoft-authentication-library-for-objc --theme fullwidth
 fi
