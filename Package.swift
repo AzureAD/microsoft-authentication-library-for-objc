47d2792482789c28a4caf5ec92fb48d493fc80ee
runner <runner@Mac-1609369127856.local>
2020-12-30 23:46:10 +0000

    Updating MSAL framework checksum & url for 1.1.14-SPMTest [skip ci]

---
+++ Package.swift
@@ -0,0 +1,16 @@
+import PackageDescription
+
+let package = Package(
+  name: "MSAL",
+  platforms: [
+        .macOS(.v10_12),.iOS(.v11)
+  ],
+  products: [
+      .library(
+          name: "MSAL",
+          targets: ["MSAL"]),
+  ],
+  targets: [
+      .binaryTarget(name: "MSAL", url: "https://github.com/AzureAD/microsoft-authentication-library-for-objc/releases/download/1.1.14-SPMTest/MSAL.zip", checksum: "03418105b2f086ee7dc06314f902dd06d89e14a528814fb768dd7894261b500e")
+  ]
+)
