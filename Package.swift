fe8c307f0a24b6f533d2c2209455fa66ad249752
Yong Zeng <zeyong@microsoft.com>
2021-02-23 18:05:05 +0000

    Updating MSAL framework checksum & url for 1.1.15 [skip ci]

--- Package.swift
+++ Package.swift
@@ -13,6 +13,6 @@ let package = Package(
           targets: ["MSAL"]),
   ],
   targets: [
-      .binaryTarget(name: "MSAL", url: "https://github.com/AzureAD/microsoft-authentication-library-for-objc/releases/download/1.1.14/MSAL.zip", checksum: "21df6a5d84ba88b9913b86a56a01b4ea17ce94517f5f751e7d3f9cedc88b668c")
+      .binaryTarget(name: "MSAL", url: "https://github.com/AzureAD/microsoft-authentication-library-for-objc/releases/download/1.1.15/MSAL.zip", checksum: "1da8180aa1928164ebd305fa7f20d2b7ba02ed5a58860e66ee2cf72c9f26875a")
   ]
 )
