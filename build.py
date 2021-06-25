3eb2980a9d3a68f9683fa6cccb1606bbdba42568
Ameya Patil <amepatil@microsoft.com>
2021-06-25 16:03:22 -0400

    Adding PR validation YML

--- build.py
+++ build.py
@@ -60,12 +60,15 @@ target_specifiers = [
 		"operations" : [ "build", "test", "codecov" ],
 		"min_warn_codecov" : 70.0,
 		"platform" : "iOS",
+        "target" : "iosFramework"
 	},
 	{
 		"name" : "iOS Test App",
 		"scheme" : "MSAL Test App (iOS)",
 		"operations" : [ "build" ],
 		"platform" : "iOS",
+        "target" : "iosTestApp"
+        
 	},
 	{
 		"name" : "Sample iOS App",
@@ -73,6 +76,7 @@ target_specifiers = [
 		"workspace" : "Samples/ios/SampleApp.xcworkspace",
 		"operations" : [ "build" ],
 		"platform" : "iOS",
+        "target" : "sampleIosApp"
 	},
     {
         "name" : "Sample iOS App-iOS",
@@ -80,22 +84,24 @@ target_specifiers = [
         "workspace" : "Samples/ios/SampleApp.xcworkspace",
         "operations" : [ "build" ],
         "platform" : "iOS",
+        "target" : "sampleIosAppSwift"
     },
 	{
 		"name" : "Mac Framework",
 		"scheme" : "MSAL (Mac Framework)",
 		"operations" : [ "build", "test", "codecov" ],
 		"min_warn_codecov" : 70.0,
-		"platform" : "Mac"
+		"platform" : "Mac",
+        "target" : "macFramework"
 	},
 ]
 
 def print_operation_start(name, operation) :
 	print ColorValues.HDR + "Beginning " + name + " [" + operation + "]" + ColorValues.END
-	print "travis_fold:start:" + (name + "_" + operation).replace(" ", "_")
+	print "##[group]" + (name + "_" + operation).replace(" ", "_")
 
 def print_operation_end(name, operation, exit_code, start_time) :
-	print "travis_fold:end:" + (name + "_" + operation).replace(" ", "_")
+	print "##[endgroup]" + (name + "_" + operation).replace(" ", "_")
 	
 	end_time = timer()
 
@@ -174,7 +180,7 @@ class BuildTarget:
 		
 		print "Retrieving Build Settings for " + self.name
 		if (show_build_settings) :
-			print "travis_fold:start:" + (self.name + "_settings").replace(" ", "_")
+			print "##[group]" + (self.name + "_settings").replace(" ", "_")
 				
 		command = self.xcodebuild_command(None, False)
 		command += " -showBuildSettings"
@@ -185,7 +191,7 @@ class BuildTarget:
 		settings_blob = subprocess.check_output(command, shell=True)
 		if (show_build_settings) :
 			print settings_blob
-			print "travis_fold:end:" + (self.name + "_settings").replace(" ", "_")
+			print "##[endgroup]" + (self.name + "_settings").replace(" ", "_")
 		
 		settings_blob = settings_blob.decode("utf-8")
 		settings_blob = settings_blob.split("\n")
@@ -429,5 +435,8 @@ if code_coverage :
 script_end_time = timer()
 
 print "Total running time: " + "{0:.2f}".format(script_end_time - script_start_time) + " seconds"
-
+# xcodebuild seems to log in stderr instead of stdout. Catching final_status in text file to capture exit code and determine if build failed
+# Similar issue : (see https://developer.apple.com/forums/thread/663959)
+status_file = open("status.txt", "w")
+status_file.write(str(final_status))
 sys.exit(final_status)
