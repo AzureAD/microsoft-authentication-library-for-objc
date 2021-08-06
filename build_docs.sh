ebebf0a90e9db37cb549c429bb4ad8cb02728b69
Ameya Patil <amepatil@microsoft.com>
2021-08-06 19:20:55 -0400

    Initial commit

---
+++ build_docs.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+gem install jazzy
+echo -e "Copying MSAL public files"
+mkdir docs.temp
+mkdir docs.temp/MSAL
+cp `find MSAL/src/public` docs.temp/MSAL
+cp README.md docs.temp/
+cd docs.temp
+echo -e "Generating MSAL documentation"
+jazzy --objc --umbrella-header MSAL/MSAL.h --framework-root . --sdk iphonesimulator --author Microsoft\ Corporation --author_url https://aka.ms/azuread  --github_url https://github.com/AzureAD/microsoft-authentication-library-for-objc --theme fullwidth
+
