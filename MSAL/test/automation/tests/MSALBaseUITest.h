870cdaf064e73c6a45be437fa3de204f15f6ff1f
Sergey Demchenko <sedemche@microsoft.com>
2021-10-12 13:14:38 -0700

    Read automation tests params from file.

--- MSAL/test/automation/tests/MSALBaseUITest.h
+++ MSAL/test/automation/tests/MSALBaseUITest.h
@@ -32,6 +32,7 @@
 #import "MSIDTestAutomationApplication.h"
 #import "MSIDTestAutomationAccountConfigurationRequest.h"
 #import "MSIDTestAutomationAccount.h"
+#import "MSIDTestsConfig.h"
 
 @interface MSALBaseUITest : XCTestCase
 
@@ -41,6 +42,7 @@
 @property (nonatomic) MSIDTestAutomationAccount *primaryAccount;
 @property (nonatomic) MSIDTestAutomationApplication *testApplication;
 @property (nonatomic) NSString *redirectUriPrefix;
+@property (nonatomic, class, readonly) MSIDTestsConfig *testsConfig;
 
 // Common checks/assertions
 - (void)assertRefreshTokenInvalidated;
