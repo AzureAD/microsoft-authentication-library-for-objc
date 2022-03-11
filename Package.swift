80af3f642dd889e3406c1c57105a065ec0144c88
Ameya Patil <amepatil@microsoft.com>
2022-03-10 18:23:30 -0800

    Updating package.swift to good.zip

--- Package.swift
+++ Package.swift
@@ -13,6 +13,6 @@ let package = Package(
           targets: ["MSAL"]),
   ],
   targets: [
-      .binaryTarget(name: "MSAL", url: "https://github.com/AzureAD/microsoft-authentication-library-for-objc/blob/ameyapat/test-spm-mac/bad.zip?raw=true", checksum: "abe672fd47ecc074831d90c2bf61fee705f0718e1ec2da1de82322a77b21da0f")
+      .binaryTarget(name: "MSAL", url: "https://github.com/AzureAD/microsoft-authentication-library-for-objc/blob/ameyapat/test-spm-mac/good.zip?raw=true", checksum: "fc454c4b993025444c582c763b178d6da716c711450fb6ef2c66318bcca2d092")
   ]
 )
