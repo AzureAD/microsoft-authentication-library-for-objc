f8cc524eb70e370899d9553a98966481d23510cc
Diego Jerez <diegoje@microsoft.com>
2023-01-10 22:05:41 +0000

    Cleanup

--- MSAL/src/CIAM/requests/parameters/MSALNativeContext.swift
+++ MSAL/src/CIAM/requests/parameters/MSALNativeContext.swift
@@ -1,4 +1,7 @@
 //
+// Copyright (c) Microsoft Corporation.
+// All rights reserved.
+//
 // This code is licensed under the MIT License.
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
@@ -13,13 +16,11 @@
 //
 // THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 // IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.IN NO EVENT SHALL THE
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
 // AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
 // LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 // THE SOFTWARE.
-//
-//------------------------------------------------------------------------------
 
 @_implementationOnly import MSAL_Private
 
