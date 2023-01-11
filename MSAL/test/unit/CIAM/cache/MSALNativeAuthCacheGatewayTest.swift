40edea03a24171bed2217c065934ff8c03f71921
Diego Jerez <diegoje@microsoft.com>
2023-01-11 08:55:00 +0000

    Remove MSALNativeAuthRequestParameters class (now the general protocol for native parameters is MSALNativeRequestable). Updated native CacheGateway tests to use a separate object for MSIDContext.

--- MSAL/test/unit/CIAM/cache/MSALNativeAuthCacheGatewayTest.swift
+++ MSAL/test/unit/CIAM/cache/MSALNativeAuthCacheGatewayTest.swift
@@ -29,9 +29,10 @@ import XCTest
 final class MSALNativeAuthCacheGatewayTest: XCTestCase {
     private let gateway = MSALNativeAuthCacheGateway()
     private lazy var parameters = getParameters()
+    private lazy var contextStub = ContextStub()
     
     override func tearDownWithError() throws {
-        try gateway.clearCache(accountIdentifier: parameters.accountIdentifier, authority: parameters.msidConfiguration.authority, clientId: parameters.clientId, context: parameters)
+        try gateway.clearCache(accountIdentifier: parameters.accountIdentifier, authority: parameters.msidConfiguration.authority, clientId: parameters.clientId, context: contextStub)
     }
     
     // MARK: happy cases
@@ -39,10 +40,10 @@ final class MSALNativeAuthCacheGatewayTest: XCTestCase {
     func testTokensStore_whenAllInfoPresent_shouldSaveTokensCorrectly() {
         let tokenResponse = getTokenResponse()
         let parameters = getParameters()
-        XCTAssertNoThrow(try gateway.saveTokensAndAccount(tokenResult: tokenResponse, configuration: parameters.msidConfiguration, context: parameters))
+        XCTAssertNoThrow(try gateway.saveTokensAndAccount(tokenResult: tokenResponse, configuration: parameters.msidConfiguration, context: contextStub))
         var tokens: MSALNativeAuthTokens? = nil
         
-        XCTAssertNoThrow(tokens = try gateway.getTokens(accountIdentifier: parameters.accountIdentifier, configuration: parameters.msidConfiguration, context: parameters))
+        XCTAssertNoThrow(tokens = try gateway.getTokens(accountIdentifier: parameters.accountIdentifier, configuration: parameters.msidConfiguration, context: contextStub))
         XCTAssertEqual(tokens?.accessToken?.accessToken, tokenResponse.accessToken)
         XCTAssertEqual(tokens?.refreshToken?.refreshToken, tokenResponse.refreshToken)
         XCTAssertEqual(tokens?.idToken?.rawIdToken, tokenResponse.idToken)
@@ -50,10 +51,10 @@ final class MSALNativeAuthCacheGatewayTest: XCTestCase {
     
     func testUpdateTokensAndAccount_whenAllInfoPresent_shouldUpdateDataCorrectly() {
         let tokenResponse = getTokenResponse()
-        XCTAssertNoThrow(try gateway.saveTokensAndAccount(tokenResult: tokenResponse, configuration: parameters.msidConfiguration, context: parameters))
+        XCTAssertNoThrow(try gateway.saveTokensAndAccount(tokenResult: tokenResponse, configuration: parameters.msidConfiguration, context: contextStub))
         var tokens: MSALNativeAuthTokens? = nil
         
-        XCTAssertNoThrow(tokens = try gateway.getTokens(accountIdentifier: parameters.accountIdentifier, configuration: parameters.msidConfiguration, context: parameters))
+        XCTAssertNoThrow(tokens = try gateway.getTokens(accountIdentifier: parameters.accountIdentifier, configuration: parameters.msidConfiguration, context: contextStub))
         XCTAssertEqual(tokens?.accessToken?.accessToken, tokenResponse.accessToken)
         XCTAssertEqual(tokens?.refreshToken?.refreshToken, tokenResponse.refreshToken)
         XCTAssertEqual(tokens?.idToken?.rawIdToken, tokenResponse.idToken)
@@ -64,9 +65,9 @@ final class MSALNativeAuthCacheGatewayTest: XCTestCase {
         tokenResponse.accessToken = newAccessToken
         tokenResponse.refreshToken = newRefreshToken
         tokenResponse.idToken = newIdToken
-        XCTAssertNoThrow(try gateway.saveTokensAndAccount(tokenResult: tokenResponse, configuration: parameters.msidConfiguration, context: parameters))
-        
-        XCTAssertNoThrow(tokens = try gateway.getTokens(accountIdentifier: parameters.accountIdentifier, configuration: parameters.msidConfiguration, context: parameters))
+        XCTAssertNoThrow(try gateway.saveTokensAndAccount(tokenResult: tokenResponse, configuration: parameters.msidConfiguration, context: contextStub))
+
+        XCTAssertNoThrow(tokens = try gateway.getTokens(accountIdentifier: parameters.accountIdentifier, configuration: parameters.msidConfiguration, context: contextStub))
         XCTAssertEqual(tokens?.accessToken?.accessToken, newAccessToken)
         XCTAssertEqual(tokens?.refreshToken?.refreshToken, newRefreshToken)
         XCTAssertEqual(tokens?.idToken?.rawIdToken, newIdToken)
@@ -74,10 +75,10 @@ final class MSALNativeAuthCacheGatewayTest: XCTestCase {
     
     func testAccountStore_whenAllInfoPresent_shouldStoreAccountCorrectly() {
         let tokenResponse = getTokenResponse()
-        XCTAssertNoThrow(try gateway.saveTokensAndAccount(tokenResult: tokenResponse, configuration: parameters.msidConfiguration, context: parameters))
-        
+        XCTAssertNoThrow(try gateway.saveTokensAndAccount(tokenResult: tokenResponse, configuration: parameters.msidConfiguration, context: contextStub))
+
         var account: MSIDAccount? = nil
-        XCTAssertNoThrow(account = try gateway.getAccount(accountIdentifier: parameters.accountIdentifier, authority: parameters.msidConfiguration.authority, context: parameters))
+        XCTAssertNoThrow(account = try gateway.getAccount(accountIdentifier: parameters.accountIdentifier, authority: parameters.msidConfiguration.authority, context: contextStub))
         XCTAssertEqual(account?.accountIdentifier.homeAccountId, parameters.accountIdentifier.homeAccountId)
         // this information was took from the TokenResponse.IDToken (JWT format)
         XCTAssertEqual(account?.accountIdentifier.displayableId, "1234567890")
@@ -88,41 +89,41 @@ final class MSALNativeAuthCacheGatewayTest: XCTestCase {
     
     func testTokensDeletion_whenAllInfoPresent_shouldRemoveTokensCorrectly() {
         var tokens: MSALNativeAuthTokens? = nil
-        XCTAssertThrowsError(tokens = try gateway.getTokens(accountIdentifier: parameters.accountIdentifier, configuration: parameters.msidConfiguration, context: parameters))
+        XCTAssertThrowsError(tokens = try gateway.getTokens(accountIdentifier: parameters.accountIdentifier, configuration: parameters.msidConfiguration, context: contextStub))
         XCTAssertNil(tokens)
         
         let tokenResponse = getTokenResponse()
-        try? gateway.saveTokensAndAccount(tokenResult: tokenResponse, configuration: parameters.msidConfiguration, context: parameters)
+        try? gateway.saveTokensAndAccount(tokenResult: tokenResponse, configuration: parameters.msidConfiguration, context: contextStub)
         
-        tokens = try? gateway.getTokens(accountIdentifier: parameters.accountIdentifier, configuration: parameters.msidConfiguration, context: parameters)
+        tokens = try? gateway.getTokens(accountIdentifier: parameters.accountIdentifier, configuration: parameters.msidConfiguration, context: contextStub)
         XCTAssertNotNil(tokens)
         
-        XCTAssertNoThrow(try gateway.removeTokens(accountIdentifier: parameters.accountIdentifier, authority: parameters.msidConfiguration.authority, clientId: parameters.msidConfiguration.clientId, context: parameters))
+        XCTAssertNoThrow(try gateway.removeTokens(accountIdentifier: parameters.accountIdentifier, authority: parameters.msidConfiguration.authority, clientId: parameters.msidConfiguration.clientId, context: contextStub))
         
-        tokens = try? gateway.getTokens(accountIdentifier: parameters.accountIdentifier, configuration: parameters.msidConfiguration, context: parameters)
+        tokens = try? gateway.getTokens(accountIdentifier: parameters.accountIdentifier, configuration: parameters.msidConfiguration, context: contextStub)
         XCTAssertNil(tokens)
     }
     
     func testClearCache_whenAllInfoPresent_shouldRemoveTokensAndAccountCorrectly() {
         var tokens: MSALNativeAuthTokens? = nil
         var account: MSIDAccount? = nil
-        XCTAssertThrowsError(tokens = try gateway.getTokens(accountIdentifier: parameters.accountIdentifier, configuration: parameters.msidConfiguration, context: parameters))
-        XCTAssertThrowsError(account = try gateway.getAccount(accountIdentifier: parameters.accountIdentifier, authority: parameters.msidConfiguration.authority, context: parameters))
+        XCTAssertThrowsError(tokens = try gateway.getTokens(accountIdentifier: parameters.accountIdentifier, configuration: parameters.msidConfiguration, context: contextStub))
+        XCTAssertThrowsError(account = try gateway.getAccount(accountIdentifier: parameters.accountIdentifier, authority: parameters.msidConfiguration.authority, context: contextStub))
         XCTAssertNil(tokens)
         XCTAssertNil(account)
         
         let tokenResponse = getTokenResponse()
-        try? gateway.saveTokensAndAccount(tokenResult: tokenResponse, configuration: parameters.msidConfiguration, context: parameters)
+        try? gateway.saveTokensAndAccount(tokenResult: tokenResponse, configuration: parameters.msidConfiguration, context: contextStub)
         
-        tokens = try? gateway.getTokens(accountIdentifier: parameters.accountIdentifier, configuration: parameters.msidConfiguration, context: parameters)
-        account = try? gateway.getAccount(accountIdentifier: parameters.accountIdentifier, authority: parameters.msidConfiguration.authority, context: parameters)
+        tokens = try? gateway.getTokens(accountIdentifier: parameters.accountIdentifier, configuration: parameters.msidConfiguration, context: contextStub)
+        account = try? gateway.getAccount(accountIdentifier: parameters.accountIdentifier, authority: parameters.msidConfiguration.authority, context: contextStub)
         XCTAssertNotNil(tokens)
         XCTAssertNotNil(account)
         
-        XCTAssertNoThrow(try gateway.clearCache(accountIdentifier: parameters.accountIdentifier, authority: parameters.msidConfiguration.authority, clientId: parameters.msidConfiguration.clientId, context: parameters))
+        XCTAssertNoThrow(try gateway.clearCache(accountIdentifier: parameters.accountIdentifier, authority: parameters.msidConfiguration.authority, clientId: parameters.msidConfiguration.clientId, context: contextStub))
         
-        tokens = try? gateway.getTokens(accountIdentifier: parameters.accountIdentifier, configuration: parameters.msidConfiguration, context: parameters)
-        account = try? gateway.getAccount(accountIdentifier: parameters.accountIdentifier, authority: parameters.msidConfiguration.authority, context: parameters)
+        tokens = try? gateway.getTokens(accountIdentifier: parameters.accountIdentifier, configuration: parameters.msidConfiguration, context: contextStub)
+        account = try? gateway.getAccount(accountIdentifier: parameters.accountIdentifier, authority: parameters.msidConfiguration.authority, context: contextStub)
         XCTAssertNil(tokens)
         XCTAssertNil(account)
     }
@@ -130,52 +131,56 @@ final class MSALNativeAuthCacheGatewayTest: XCTestCase {
     // MARK: unhappy cases
     
     func testDataRetrieval_whenNoDataIsStored_shouldThrowsAnError() {
-        XCTAssertThrowsError(try gateway.getTokens(accountIdentifier: parameters.accountIdentifier, configuration: parameters.msidConfiguration, context: parameters))
-        XCTAssertThrowsError(try gateway.getAccount(accountIdentifier: parameters.accountIdentifier, authority: parameters.msidConfiguration.authority, context: parameters))
+        XCTAssertThrowsError(try gateway.getTokens(accountIdentifier: parameters.accountIdentifier, configuration: parameters.msidConfiguration, context: contextStub))
+        XCTAssertThrowsError(try gateway.getAccount(accountIdentifier: parameters.accountIdentifier, authority: parameters.msidConfiguration.authority, context: contextStub))
     }
     
     func testContentDeletion_whenNoDataIsStored_shouldNotThrowsAnError() {
-        XCTAssertNoThrow(try gateway.removeTokens(accountIdentifier: parameters.accountIdentifier, authority: parameters.msidConfiguration.authority, clientId: parameters.msidConfiguration.clientId, context: parameters))
-        XCTAssertNoThrow(try gateway.clearCache(accountIdentifier: parameters.accountIdentifier, authority: parameters.msidConfiguration.authority, clientId: parameters.msidConfiguration.clientId, context: parameters))
+        XCTAssertNoThrow(try gateway.removeTokens(accountIdentifier: parameters.accountIdentifier, authority: parameters.msidConfiguration.authority, clientId: parameters.msidConfiguration.clientId, context: contextStub))
+        XCTAssertNoThrow(try gateway.clearCache(accountIdentifier: parameters.accountIdentifier, authority: parameters.msidConfiguration.authority, clientId: parameters.msidConfiguration.clientId, context: contextStub))
     }
     
     func testRemoveTokens_whenInvalidInputIsUsed_shouldNotThrowsAnError() {
         var authority: MSIDAuthority? = nil
         XCTAssertNoThrow(authority = try MSIDB2CAuthority(url: URL(string: "https://www.microsoft.com")!, validateFormat: false, context: nil))
-        XCTAssertNoThrow(try gateway.removeTokens(accountIdentifier: MSIDAccountIdentifier(), authority: authority!, clientId: "" , context: parameters))
+        XCTAssertNoThrow(try gateway.removeTokens(accountIdentifier: MSIDAccountIdentifier(), authority: authority!, clientId: "" , context: contextStub))
     }
     
     func testStoreTokens_whenAccessAndRefreshTokensAreMissing_shouldThrowsAnErrorOnGetTokens() {
         let tokenResponse = getTokenResponse()
         tokenResponse.accessToken = nil
         tokenResponse.refreshToken = nil
-        XCTAssertNoThrow(try gateway.saveTokensAndAccount(tokenResult: tokenResponse, configuration: parameters.msidConfiguration, context: parameters))
+        XCTAssertNoThrow(try gateway.saveTokensAndAccount(tokenResult: tokenResponse, configuration: parameters.msidConfiguration, context: contextStub))
         
-        XCTAssertThrowsError(try gateway.getTokens(accountIdentifier: parameters.accountIdentifier, configuration: parameters.msidConfiguration, context: parameters))
+        XCTAssertThrowsError(try gateway.getTokens(accountIdentifier: parameters.accountIdentifier, configuration: parameters.msidConfiguration, context: contextStub))
     }
     
     func testGetTokens_whenThereIsNoAuthSchemeOrAccountIdentifier_shouldThrowsAnError() {
         let tokenResponse = getTokenResponse()
-        XCTAssertNoThrow(try gateway.saveTokensAndAccount(tokenResult: tokenResponse, configuration: parameters.msidConfiguration, context: parameters))
+        XCTAssertNoThrow(try gateway.saveTokensAndAccount(tokenResult: tokenResponse, configuration: parameters.msidConfiguration, context: contextStub))
         
         var parameters = getParameters()
         parameters.msidConfiguration.authScheme = nil
-        XCTAssertThrowsError(try gateway.getTokens(accountIdentifier: parameters.accountIdentifier, configuration: parameters.msidConfiguration, context: parameters))
+        XCTAssertThrowsError(try gateway.getTokens(accountIdentifier: parameters.accountIdentifier, configuration: parameters.msidConfiguration, context: contextStub))
         
         parameters = getParameters()
         parameters.accountIdentifier = MSIDAccountIdentifier()
-        XCTAssertThrowsError(try gateway.getTokens(accountIdentifier: parameters.accountIdentifier, configuration: parameters.msidConfiguration, context: parameters))
+        XCTAssertThrowsError(try gateway.getTokens(accountIdentifier: parameters.accountIdentifier, configuration: parameters.msidConfiguration, context: contextStub))
     }
     
     // MARK: private methods
     
-    private func getParameters() -> MSALNativeAuthRequestParameters {
-        let parameters = MSALNativeAuthRequestParameters()
-        parameters.msidConfiguration = getMSIDConfiguration()
-        parameters.accountIdentifier = getAccountIdentifier()
-        parameters.clientId = "clientId"
-        parameters.oidcScope = "user.read"
-        return parameters
+    private func getParameters() -> ParametersStub {
+        ParametersStub(
+            tenant: .init(string: DEFAULT_TEST_AUTHORITY)!,
+            clientId: "clientId",
+            endpoint: .signUp,
+            context: MSIDBasicContext(),
+            correlationId: .init(),
+            url: .init(string: DEFAULT_TEST_RESOURCE)!,
+            accountIdentifier: getAccountIdentifier(),
+            msidConfiguration: getMSIDConfiguration()
+        )
     }
     
     private func getAccountIdentifier() -> MSIDAccountIdentifier {
@@ -202,5 +207,48 @@ final class MSALNativeAuthCacheGatewayTest: XCTestCase {
         ])
         configuration.authScheme = authSchema
         return configuration
+    }
+}
+
+private struct ParametersStub: MSALNativeRequestable {
+    let tenant: URL
+    let clientId: String
+    let endpoint: MSAL.MSALNativeEndpoint
+    let context: MSIDRequestContext
+    let correlationId: UUID
+    let url: URL
+    var accountIdentifier: MSIDAccountIdentifier
+    let msidConfiguration: MSIDConfiguration
+}
+
+private class ContextStub: MSIDRequestContext {
+
+    var currentAppRequestMetadata = [AnyHashable : Any]()
+    var internalCorrelationId = UUID()
+    var telemetryId = UUID()
+
+    init() {
+        guard let metadata = Bundle.main.infoDictionary else { return }
+        let appName = metadata["CFBundleDisplayName"] ?? (metadata["CFBundleName"] ?? "")
+        let appVer = metadata["CFBundleShortVersionString"] ?? ""
+        currentAppRequestMetadata[MSID_VERSION_KEY] = MSIDVersion.sdkVersion()
+        currentAppRequestMetadata[MSID_APP_NAME_KEY] = appName
+        currentAppRequestMetadata[MSID_APP_VER_KEY] = appVer
+    }
+
+    func correlationId() -> UUID! {
+        internalCorrelationId
+    }
+
+    func logComponent() -> String! {
+        MSIDVersion.sdkName()
+    }
+
+    func telemetryRequestId() -> String! {
+        telemetryId.uuidString
+    }
+
+    func appRequestMetadata() -> [AnyHashable : Any]! {
+        currentAppRequestMetadata
     }
 }
