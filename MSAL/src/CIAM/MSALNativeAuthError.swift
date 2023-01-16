88a6ea57568a6b5319d209ea13c468b30f3a57cb
Silviu Petrescu <spetrescu@microsoft.com>
2023-01-16 10:58:01 +0000

    Removed incorrect file, reverted to previous format

--- MSAL/src/CIAM/MSALNativeAuthError.swift
+++ MSAL/src/CIAM/MSALNativeAuthError.swift
@@ -22,6 +22,7 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 // THE SOFTWARE.
 
+
 enum MSALNativeAuthError: Error, Equatable {
     case validationError
     case tokenResultNotPresent
