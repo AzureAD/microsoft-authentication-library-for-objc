6a9c6612d03e20e3f000a0d214f3732efa9fe3fd
Danilo Raspa <daniloraspa@microsoft.com>
2023-03-27 21:10:36 +0100

    creating first draft of external state machine for signup

--- MSAL/src/NativeAuth/public/MSALNativeAuthPublicClientApplication.swift
+++ MSAL/src/NativeAuth/public/MSALNativeAuthPublicClientApplication.swift
@@ -24,6 +24,167 @@
 
 import Foundation
 
+
+@objcMembers
+public class MSALNativeError: LocalizedError {
+    private let message: String?
+    
+    init(message: String? = nil) {
+        self.message = message
+    }
+    
+    public var errorDescription: String? {
+        message
+    }
+}
+
+@objcMembers
+public class SignUpError: MSALNativeError {
+    let type: SignUpErrorType
+    
+    init(type: SignUpErrorType, message: String? = nil) {
+        self.type = type
+        super.init(message: message)
+    }
+}
+
+@objcMembers
+public class VerifyCodeError: MSALNativeError {
+    let type: VerifyCodeErrorType
+    
+    init(type: VerifyCodeErrorType, message: String? = nil) {
+        self.type = type
+        super.init(message: message)
+    }
+}
+
+@objcMembers
+public class PasswordRequiredError: MSALNativeError {
+    let type: PasswordRequiredErrorType
+    
+    init(type: PasswordRequiredErrorType, message: String? = nil) {
+        self.type = type
+        super.init(message: message)
+    }
+}
+
+@objcMembers
+public class AttributeRequiredError: MSALNativeError {
+    let type: AttributeRequiredErrorType
+    
+    init(type: AttributeRequiredErrorType, message: String? = nil) {
+        self.type = type
+        super.init(message: message)
+    }
+}
+
+//TODO: is this a good name?
+@objc
+public enum SignUpErrorType: Int {
+    case userExists
+    case passwordValidation
+    case invalidAttributes
+    case generalError
+}
+
+@objc
+public enum VerifyCodeErrorType: Int {
+    case generalError
+    case invalidOOB
+}
+
+@objc
+public enum PasswordRequiredErrorType: Int {
+    case generalError
+    case invalidPassword
+}
+
+@objc
+public enum AttributeRequiredErrorType: Int {
+    case generalError
+    case invalidAttribute
+}
+
+public protocol SignUpDelegate {
+    func onOOBSent(flow: OOBSentFlow)
+    func onError(error: SignUpError)
+    func onRedirect()
+}
+
+public protocol VerifyCodeDelegate {
+    //TODO: what should we return here? SLT?
+    func completed()
+    //TODO: do we need the state for the error? can the ext dev use the existing flow instance?
+    func onError(error: VerifyCodeError, state: OOBSentFlow?)
+    //TODO: do we need this method for verifyCode?
+    func onRedirect()
+    func passwordRequired(flow: PasswordRequiredFlow)
+    //question for Rodhan, can we fall here after email verification?
+    func attributesRequired(flow: AttributeRequiredFlow)
+}
+
+public protocol PasswordRequiredDelegate {
+    func completed()
+    //TODO: do we need the state for the error? can the ext dev use the existing flow instance?
+    func onError(error: PasswordRequiredError, state: PasswordRequiredFlow?)
+    //TODO: do we need this method for passwordRequired?
+    func onRedirect()
+    func attributesRequired(flow: AttributeRequiredFlow)
+}
+
+public protocol AttributeRequiredDelegate {
+    func completed()
+    //TODO: do we need the state for the error? can the ext dev use the existing flow instance?
+    func onError(error: AttributeRequiredError, state: AttributeRequiredFlow?)
+    //TODO: do we need this method for passwordRequired?
+    func onRedirect()
+}
+
+//TODO: create super class
+@objcMembers
+public class OOBSentFlow {
+    private let credentialToken: String
+
+    init(credentialToken: String) {
+        self.credentialToken = credentialToken
+    }
+
+    // TODO: we need a delegate to manage unexpected errors, maybe we need a new delegate to manage less errors than signIn and not redirect
+    public func resendCode(delegate: SignUpDelegate, correlationId: UUID? = nil) {
+        delegate.onOOBSent(flow: self)
+    }
+
+    public func verifyCode(otp: String, flow: VerifyCodeDelegate, correlationId: UUID? = nil) {
+        flow.completed()
+    }
+}
+
+@objcMembers
+public class PasswordRequiredFlow {
+    private let credentialToken: String
+
+    init(credentialToken: String) {
+        self.credentialToken = credentialToken
+    }
+
+    public func setPassword(password: String, flow: PasswordRequiredDelegate, correlationId: UUID? = nil) {
+        
+    }
+}
+
+@objcMembers
+public class AttributeRequiredFlow {
+    private let credentialToken: String
+
+    init(credentialToken: String) {
+        self.credentialToken = credentialToken
+    }
+
+    public func setAttributes(attributes: [String: Any], flow: AttributeRequiredDelegate, correlationId: UUID? = nil) {
+        
+    }
+}
+
 @objcMembers
 public final class MSALNativeAuthPublicClientApplication: MSALPublicClientApplication {
 
@@ -77,7 +238,13 @@ public final class MSALNativeAuthPublicClientApplication: MSALPublicClientApplic
 
         super.init()
     }
-
+    
+    // MARK: new methods
+    
+    public func signUp(email: String, password: String?, attributes: [String: Any], correlationId: UUID?, callback: SignUpDelegate) {
+        callback.onError(error: SignUpError(type: SignUpErrorType.userExists))
+    }
+    
     // MARK: - Async/Await
 
     public func signUp(parameters: MSALNativeAuthSignUpParameters) async -> AuthResult {
