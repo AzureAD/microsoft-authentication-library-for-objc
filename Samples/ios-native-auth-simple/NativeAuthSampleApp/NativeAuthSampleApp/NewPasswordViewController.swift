25751d77a4cd88029d014e42a98cebabb848167f
Rodhan Hickey <rodhanhickey@microsoft.com>
2023-03-30 15:40:40 +0100

    Updates and tidying

--- Samples/ios-native-auth-simple/NativeAuthSampleApp/NativeAuthSampleApp/NewPasswordViewController.swift
+++ Samples/ios-native-auth-simple/NativeAuthSampleApp/NativeAuthSampleApp/NewPasswordViewController.swift
@@ -1,15 +1,32 @@
 //
-//  NewPasswordViewController.swift
-//  NativeAuthSampleApp
+// Copyright (c) Microsoft Corporation.
+// All rights reserved.
 //
-//  Created by Rodhan Hickey on 28/03/2023.
+// This code is licensed under the MIT License.
 //
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files(the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and / or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions :
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
 
 import UIKit
 
 class NewPasswordViewController: UIViewController {
 
-    var passwordSubmittedCallback: ((_ otp: String)->())?
+    var passwordSubmittedCallback: ((_ otp: String) -> Void)?
 
     @IBOutlet weak var errorLabel: UILabel!
     @IBOutlet weak var passwordTextField: UITextField!
