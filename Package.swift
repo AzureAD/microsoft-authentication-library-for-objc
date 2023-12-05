6b8f2c885da66344b4f93ac95cda3a2fffab43c4
Silviu Petrescu <spetrescu@microsoft.com>
2023-12-05 10:58:31 +0000

    New zip & SPM

--- Package.swift
+++ Package.swift
@@ -13,6 +13,6 @@ let package = Package(
           targets: ["MSAL"]),
   ],
   targets: [
-      .binaryTarget(name: "MSAL", url: "https://github.com/AzureAD/microsoft-authentication-library-for-objc/raw/spetrescu/january-release-testspm/MSAL.zip", checksum: "a6d3bb5a549a63f02f8e00ec177b317d5586b24250f581c8c6dd5463185d15c0")
+      .binaryTarget(name: "MSAL", url: "https://github.com/AzureAD/microsoft-authentication-library-for-objc/releases/download/1.2.18/MSAL.zip", checksum: "82c28cdd804d4bf96687c7067b1d34b0af291343fa517efb859fc9b502dc0292")
   ]
 )
