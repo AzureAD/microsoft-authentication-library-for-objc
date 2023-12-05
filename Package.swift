fe73d1ada9e4e0e0ae1fa60a97a731566842da52
Silviu Petrescu <spetrescu@microsoft.com>
2023-12-05 10:31:16 +0000

    New Checksum

--- Package.swift
+++ Package.swift
@@ -13,6 +13,6 @@ let package = Package(
           targets: ["MSAL"]),
   ],
   targets: [
-      .binaryTarget(name: "MSAL", url: "https://github.com/AzureAD/microsoft-authentication-library-for-objc/raw/spetrescu/january-release-testspm/MSAL.zip", checksum: "a6d3bb5a549a63f02f8e00ec177b317d5586b24250f581c8c6dd5463185d15c0")
+      .binaryTarget(name: "MSAL", url: "https://github.com/AzureAD/microsoft-authentication-library-for-objc/raw/spetrescu/january-release-testspm/MSAL.zip", checksum: "27d05dc079199acc1eff69a43dfc04ed4704dbad7f5dc3de1229a283c644cae3")
   ]
 )
