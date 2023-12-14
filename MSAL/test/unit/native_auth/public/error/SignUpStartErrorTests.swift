31358060fe94bb7a1593e4e1c3975b294e4b6f29
Marcos Borges <marcosborges@microsoft.com>
2023-12-14 17:23:49 +0000

    - Fixed unit test.

--- MSAL/test/unit/native_auth/public/error/SignUpStartErrorTests.swift
+++ MSAL/test/unit/native_auth/public/error/SignUpStartErrorTests.swift
@@ -30,7 +30,7 @@ final class SignUpStartErrorTests: XCTestCase {
     private var sut: SignUpStartError!
 
     func test_totalCases() {
-        XCTAssertEqual(SignUpStartError.ErrorType.allCases.count, 4)
+        XCTAssertEqual(SignUpStartError.ErrorType.allCases.count, 5)
     }
 
     func test_customErrorDescription() {
