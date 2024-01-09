6df2203cc5c7497ac53b8a8b419944640ccb599a
Silviu Petrescu <spetrescu@microsoft.com>
2024-01-09 10:47:34 +0000

    Added package.swift + zip

--- Package.swift
+++ Package.swift
@@ -13,6 +13,6 @@ let package = Package(
           targets: ["MSAL"]),
   ],
   targets: [
-      .binaryTarget(name: "MSAL", url: "https://github.com/AzureAD/microsoft-authentication-library-for-objc/releases/download/1.2.19/MSAL.zip", checksum: "a90f11aac4f1a76bd93938f7bbe9f3ad375bd0e3f838dc65edff91111793701f")
+      .binaryTarget(name: "MSAL", url: "https://github.com/AzureAD/microsoft-authentication-library-for-objc/raw/spetrescu/january-release-testspm/MSAL.zip", checksum: "7d580e78db5ded41a313eb0a5ee29274b8e672f4f25e50d22ec0deeafc8507dd")
   ]
 )
