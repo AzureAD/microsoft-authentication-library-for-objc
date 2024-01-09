edf62f65aeb12318d120f7346846fec88759ad96
Silviu Petrescu <spetrescu@microsoft.com>
2024-01-09 10:58:52 +0000

    Correct URL

--- Package.swift
+++ Package.swift
@@ -13,6 +13,6 @@ let package = Package(
           targets: ["MSAL"]),
   ],
   targets: [
-      .binaryTarget(name: "MSAL", url: "https://github.com/AzureAD/microsoft-authentication-library-for-objc/raw/spetrescu/january-release-testspm/MSAL.zip", checksum: "7d580e78db5ded41a313eb0a5ee29274b8e672f4f25e50d22ec0deeafc8507dd")
+      .binaryTarget(name: "MSAL", url: "https://github.com/AzureAD/microsoft-authentication-library-for-objc/raw/spetrescu/spetrescu/ciam-new-apis-changes-for-pp-testspm/MSAL.zip", checksum: "7d580e78db5ded41a313eb0a5ee29274b8e672f4f25e50d22ec0deeafc8507dd")
   ]
 )
