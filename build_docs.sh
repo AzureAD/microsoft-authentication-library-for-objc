91a1449c595e144cdc4378aadd4ce0d4990c8716
Silviu Petrescu <spetrescu@microsoft.com>
2024-01-17 09:58:48 +0000

    Testing no changes

--- build_docs.sh
+++ build_docs.sh
@@ -5,20 +5,11 @@
 # details see https://github.com/realm/jazzy?tab=readme-ov-file#mixed-objective-c--swift
 gem install jazzy
 echo -e "Copying MSAL public files"
-mkdir -m 777 docs.temp
-mkdir -m 777 docs.temp/MSAL
+mkdir docs.temp
+mkdir docs.temp/MSAL
 cp `find MSAL/src/public` docs.temp/MSAL
-cp `find MSAL/src/native_auth/public` docs.temp/MSAL
 cp README.md docs.temp/
-
-echo -e "Generating MSAL documentation"
-# Generate Swift SourceKitten output
-sourcekitten doc -- -workspace MSAL.xcworkspace -scheme "MSAL (iOS Framework)" -configuration Debug RUN_CLANG_STATIC_ANALYZER=NO -sdk iphonesimulator CODE_SIGN_IDENTITY="" CODE_SIGNING_REQUIRED=NO -destination 'platform=iOS Simulator' > docs.temp/swiftDoc.json
-
-# Generate Objective-C SourceKitten output
 cd docs.temp
-sourcekitten doc --objc $(pwd)/MSAL/MSAL.h -- -x objective-c  -isysroot $(xcrun --show-sdk-path --sdk iphonesimulator) \-I $(pwd) -fmodules > objcDoc.json
-cd ..
+echo -e "Generating MSAL documentation"
+jazzy --objc --umbrella-header MSAL/MSAL.h --framework-root . --sdk iphonesimulator --author Microsoft\ Corporation --author_url https://aka.ms/azuread  --github_url https://github.com/AzureAD/microsoft-authentication-library-for-objc --theme fullwidth
 
-# Feed both outputs to Jazzy as a comma-separated list
-jazzy --module MSAL --sourcekitten-sourcefile docs.temp/swiftDoc.json,docs.temp/objcDoc.json --author Microsoft\ Corporation --author_url https://aka.ms/azuread --github_url https://github.com/AzureAD/microsoft-authentication-library-for-objc --theme fullwidth
