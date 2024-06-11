6fc3f6cd08512f47f3c3b90200370792caec21f9
Diego Jerez <diegoje@microsoft.com>
2024-06-11 18:28:47 +0100

    wip

--- spm-integration-test.sh
+++ spm-integration-test.sh
@@ -56,4 +56,4 @@ rm -f NativeAuthSampleApp.xcodeproj/project.xcworkspace/xcshareddata/swiftpm/Pac
 echo "Running the Sample App with the temporary Swift Package"
 
 xcodebuild -resolvePackageDependencies
-xcodebuild -scheme NativeAuthSampleApp -configuration Release -sdk iphonesimulator -destination 'platform=iOS Simulator,name=iPhone 14,OS=17.0' clean build
+xcodebuild -scheme NativeAuthSampleApp -configuration Release -sdk iphonesimulator -destination 'platform=iOS Simulator,name=iPhone 14,OS=latest' clean build
