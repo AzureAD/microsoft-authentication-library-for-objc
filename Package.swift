40416621033372cf920ff8eb19ed04a261a359fe
Silviu Petrescu <spetrescu@microsoft.com>
2024-08-28 17:49:09 +0100

    new spm & checksum

--- Package.swift
+++ Package.swift
@@ -13,6 +13,6 @@ let package = Package(
           targets: ["MSAL"]),
   ],
   targets: [
-      .binaryTarget(name: "MSAL", url: "https://github.com/AzureAD/microsoft-authentication-library-for-objc/raw/spetrescu/macos-support-spm/MSAL.zip", checksum: "369a230b4bb59d13b9704dbcb523aa648d002e18ec179544ca43c6a9f458f859")
+      .binaryTarget(name: "MSAL", url: "https://github.com/AzureAD/microsoft-authentication-library-for-objc/raw/spetrescu/macos-support-spm/MSAL.zip", checksum: "280477566f8752966151f4fb798971680a9f302bee03d098a246e1da89d408c6")
   ]
 )
