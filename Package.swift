0b790673ad7416e0e37edb32c7bcdc0375a33cf4
Veena Soman <veenasoman@microsoft.com>
2024-10-16 10:15:00 -0700

    fix SPM to point to 10.15

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 let package = Package(
   name: "MSAL",
   platforms: [
-        .macOS(.v10_13),.iOS(.v14)
+        .macOS(.v10_15),.iOS(.v14)
   ],
   products: [
       .library(
