04467b4800094ba982d886293cd7b18e24847a2b
Silviu Petrescu <111577419+spetrescu84@users.noreply.github.com>
2025-01-21 14:13:32 +0000

    Update MSAL/src/native_auth/public/parameters/MSALNativeAuthSignInAfterSignUpParameters.swift
    Co-authored-by: Danilo Raspa <105228698+nilo-ms@users.noreply.github.com>

--- MSAL/src/native_auth/public/parameters/MSALNativeAuthSignInAfterSignUpParameters.swift
+++ MSAL/src/native_auth/public/parameters/MSALNativeAuthSignInAfterSignUpParameters.swift
@@ -22,7 +22,7 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 // THE SOFTWARE.  
 
-/// Encapsulates the parameters passed to the signIn methods after signUp
+/// Encapsulates the parameters passed to the signIn method after signUp
 @objcMembers
 public class MSALNativeAuthSignInAfterSignUpParameters: NSObject {
 
