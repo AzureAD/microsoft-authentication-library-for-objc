6865ba2c3de0b84135f84f121d91ee7add93bc0e
runner <runner@Mac-1747835706019.local>
2025-05-21 14:19:18 +0000

    Publish temporary Swift Package 2025-05-21 14:09:52

--- Package.swift
+++ Package.swift
@@ -13,6 +13,6 @@ let package = Package(
           targets: ["MSAL"]),
   ],
   targets: [
-      .binaryTarget(name: "MSAL", url: "https://github.com/AzureAD/microsoft-authentication-library-for-objc/releases/download/1.9.0/MSAL.zip", checksum: "357503f594eab1adb7a7f992534d5b750329218eac33ac8b7dc6f4cee5a62219")
+      .binaryTarget(name: "MSAL", url: "https://github.com/AzureAD/microsoft-authentication-library-for-objc/raw/replace-jit-with-registerstrongauth-temp/MSAL.zip", checksum: "1144d48e88375baccebc674968441a43972d1b1c5ba19ab2bf4babd35fa03ade")
   ]
 )
