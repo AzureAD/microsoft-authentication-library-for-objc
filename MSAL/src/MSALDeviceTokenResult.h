f2ea74ab1d34996eca5d631f127487b5b99c825a
Ameya <amepatil@microsoft.com>
2026-04-24 14:52:59 -0700

    Code review feedback for ameyapat/add-get-device-token-api vs dev

--- MSAL/src/MSALDeviceTokenResult.h
+++ MSAL/src/MSALDeviceTokenResult.h
@@ -22,6 +22,11 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 // THE SOFTWARE.
 
+#import <Foundation/Foundation.h>
+
+@class MSALAuthority;
+@class MSIDTokenResult;
+
 NS_ASSUME_NONNULL_BEGIN
 
 @interface MSALDeviceTokenResult : NSObject
