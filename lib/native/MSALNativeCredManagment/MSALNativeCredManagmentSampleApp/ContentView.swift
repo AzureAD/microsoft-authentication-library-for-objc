d18fd00235f8fbafb23d79bbed4187dc123c08de
Sergey Demchenko <sedemche@microsoft.com>
2026-05-27 21:41:51 -0700

    Add sample app.

---
+++ lib/native/MSALNativeCredManagment/MSALNativeCredManagmentSampleApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MSALNativeCredManagmentSampleApp
+//
+//  Created by Serhii Demchenko on 2026-05-27.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ lib/native/MSALNativeCredManagment/MSALNativeCredManagmentSampleApp/MSALNativeCredManagmentSampleAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MSALNativeCredManagmentSampleAppApp.swift
+//  MSALNativeCredManagmentSampleApp
+//
+//  Created by Serhii Demchenko on 2026-05-27.
+//
+
+import SwiftUI
+
+@main
+struct MSALNativeCredManagmentSampleAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
