6815fa5bc10e5d1dccdf345419b20a3d80b34b0b
Silviu Petrescu <spetrescu@microsoft.com>
2026-07-22 11:56:11 +0100

    Removed import

--- MSAL/src/native_auth/controllers/v2/MSALNativeAuthFlowController.swift
+++ MSAL/src/native_auth/controllers/v2/MSALNativeAuthFlowController.swift
@@ -22,8 +22,6 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 // THE SOFTWARE.
 
-import Foundation
-
 @_implementationOnly import MSAL_Private
 
 // swiftlint:disable file_length
